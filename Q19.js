1b6895d44d4b1af0f1fc9f6e0e7ef6383bff083e
Anusha Kashif <160410652+Anushakashif@users.noreply.github.com>
2024-03-21 16:08:24 -0700

    Add files via upload

---
+++ Q19.js
@@ -0,0 +1,6 @@
+"use strict";
+// 19. Dinner Guests: Working with one of the programs from Exercise 14 through 18, print a message indicating the number 
+// of people you are inviting to dinner.
+let invitations = ["yusra", "ali"];
+let count_invitation = invitations.length;
+console.log(` ${count_invitation} people will come to the dinnner`);
---
+++ Q19.ts
@@ -0,0 +1,7 @@
+// 19. Dinner Guests: Working with one of the programs from Exercise 14 through 18, print a message indicating the number 
+// of people you are inviting to dinner.
+
+let invitations: string[] = ["yusra", "ali"]
+let count_invitation: number = invitations.length
+
+console.log(` ${count_invitation} people will come to the dinnner`)
